b77729f68efac6daa551a176fe424efba784d92d
SolihovAbdulloh <abdullasolikh@mail.com>
2025-01-19 14:22:44 +0500

    s

--- src/generic/loader/index.tsx
+++ src/generic/loader/index.tsx
@@ -1,10 +1,10 @@
-import { Skeleton } from "antd";
+import { Avatar, Skeleton } from "antd";
 
 const useLoader = () => {
   const Category_loader = () => {
     return Array.from({ length: 8 }).map((_, idx) => (
-      <div  key={idx} >
-        <Skeleton.Input  active />
+      <div key={idx}>
+        <Skeleton.Input active />
       </div>
     ));
   };
@@ -13,13 +13,21 @@ const useLoader = () => {
     return Array.from({ length: 6 }).map((_, idx) => (
       <div className="flex-col flex items-start gap-2   " key={idx}>
         <Skeleton.Image className="!w-[270px] !h-[250px]" active />
-        <Skeleton.Input  active />
-        <Skeleton.Input  active />
+        <Skeleton.Input active />
+        <Skeleton.Input active />
       </div>
     ));
-
   };
-  return { Category_loader, Card_Loader };
+  const BlogLoader = () => {
+    <div className="flex items-center ">
+      <Avatar />
+      <div>
+        <Skeleton.Input />
+        <Skeleton.Input />
+      </div>
+    </div>;
+  };
+  return { Category_loader, Card_Loader, BlogLoader };
 };
 
 export default useLoader;
--- src/pages/blog/Usercomments/index.tsx
+++ src/pages/blog/Usercomments/index.tsx
@@ -2,9 +2,13 @@ import type { FC } from "react";
 import { useParams } from "react-router-dom";
 import { useQueryApi } from "../../../hooks/useQuery";
 import { DataUserinfo, UserTypeApi } from "../../../@types";
-import { Avatar, Button } from "antd";
-import { EyeOutlined, HeartOutlined, MessageOutlined, ShareAltOutlined } from "@ant-design/icons";
-
+import { Avatar } from "antd";
+import {
+  EyeOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  ShareAltOutlined,
+} from "@ant-design/icons";
 const User_comments_blog: FC = () => {
   const { id, user_id } = useParams();
 
@@ -22,44 +26,52 @@ const User_comments_blog: FC = () => {
     pathname: "user",
     url: `/user/by_id/${user_id}`,
   });
-
+  let loading = isLoadingUser || isErrorUser || isLoading || isError;
   return (
-    <section className="w-[70%] m-auto mt-[30px]">
-      <div className="flex items-center justify-between">
-        <div className="flex  gap-5 items-center">
-          <Avatar className="w-[50px] h-[50px]" src={user?.profile_photo} />
-          <div className="flex flex-col justify-start items-center gap-1">
-            <h2 className="font-bold text-[18px]">
-              {user?.name} {user?.surname}
-            </h2>
-            <p className="text-[12px]">Followers {user?.followers?.length}</p>
+    <div>
+      {loading ? (
+        <h3>Loading</h3>
+      ) : (
+        <section className="w-[70%] m-auto mt-[30px]">
+          <div className="flex items-center justify-between">
+            <div className="flex  gap-5 items-center">
+              <Avatar className="w-[50px] h-[50px]" src={user?.profile_photo} />
+              <div className="flex flex-col justify-start items-center gap-1">
+                <h2 className="font-bold text-[18px]">
+                  {user?.name} {user?.surname}
+                </h2>
+                <p className="text-[12px]">
+                  Followers {user?.followers?.length}
+                </p>
+              </div>
+            </div>
+            <button className="bg-[#46A358] flex rounded-md items-center justify-center gap-1 text-base text-white px-[20px] py-[8px]">
+              Follow
+            </button>
           </div>
-        </div>
-        <button className="bg-[#46A358] flex rounded-md items-center justify-center gap-1 text-base text-white px-[20px] py-[8px]">
-          Follow
-        </button>
-      </div>
-      <div className="mt-7">
-        <h2 className="py-[20px] font-bold text-[24px]">{data?.title}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: data?.content as string }}
-        ></div>
-      </div>
-      <div className="flex items-center gap-4">
-        <div className="flex items-center gap-1  cursor-pointer">
-          <EyeOutlined /> <p>{data?.views}</p>
-        </div>
-        <div className="flex items-center gap-1  cursor-pointer">
-          <HeartOutlined /> <p>0</p>
-        </div>
-        <div className="flex items-center gap-1  cursor-pointer">
-          <MessageOutlined /> <p>0</p>
-        </div>
-        <div className="flex items-center gap-1 cursor-pointer">
-          <ShareAltOutlined /> <p>0</p>
-        </div>
-      </div>
-    </section>
+          <div className="mt-7">
+            <h2 className="py-[20px] font-bold text-[24px]">{data?.title}</h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: data?.content as string }}
+            ></div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1  cursor-pointer">
+              <EyeOutlined /> <p>{data?.views}</p>
+            </div>
+            <div className="flex items-center gap-1  cursor-pointer">
+              <HeartOutlined /> <p>0</p>
+            </div>
+            <div className="flex items-center gap-1  cursor-pointer">
+              <MessageOutlined /> <p>0</p>
+            </div>
+            <div className="flex items-center gap-1 cursor-pointer">
+              <ShareAltOutlined /> <p>0</p>
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
